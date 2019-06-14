12faca30e81dc9fbabe0a2a2096249fac8fb9081
matheus <matheusholscher@gmail.com>
2019-06-13 22:59:58 -0300

    estabelecendo retorno para a api da temperatura atual

--- app/controllers/busca_temp_controller.rb
+++ app/controllers/busca_temp_controller.rb
@@ -2,6 +2,10 @@ class BuscaTempController < ApplicationController
     require 'net/http'
     require 'json'
 
+    def atual
+        render json: AtualWeather.new.atual(tempe_params[:cidade]), status: :ok
+    end
+
     private
 
     def temp_params
--- app/services/atual.rb
+++ app/services/atual.rb
@@ -7,5 +7,21 @@ class AtualWeather
         url = "http://api.openweathermap.org/data/2.5/weather?q=#{@cidade},BR&appid=a5ea6d1d5826d727e19d2d84e016e048&units=metric"
     
         retorno = JSON.parse(Net::HTTP.get(URI(url)))
+
+        if retorno["message"]
+            return {message: "Cidade inexistente!"}
+        else
+            temperatura = {
+            "Graus": retorno["main"]["temp"],
+            "Pressão": retorno["main"]["pressure"],
+            "Umidade": retorno["main"]["humidity"],
+            "Temperatura máxima": retorno["main"]["temp_max"],
+            "Temperatura mínima": retorno["main"]["temp_min"],
+            "Vento": retorno["wind"]["speed"],
+            "Rajada": retorno["wind"]["deg"]
+            "Alvorada": Time.at(retorno["sys"]["sunrise"]),
+            "Crepusculo": Time.at(retorno["sys"]["sunset"]),
+            }
+        end
     end
 end
